922c8d7d853b14b70b42fc48f74af93bd43b0431
PedroFlorenzano <pedroflorenzano.dev@gmail.com>
2023-09-21 22:29:30 -0300

    Adicionando conteúdo principal da terefa

---
+++ src/main/java/com/example/tarefa1/model/Categoria.java
@@ -0,0 +1,40 @@
+package com.example.tarefa1.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "tbl_categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cat_nome",length = 200, nullable = false)
+    private String  nome;
+
+    // @OneToMany(mappedBy = "categoriaProdutos", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "categoriaProdutos")
+    private List<Produto> produtos;
+
+    public Categoria(String nome){
+        this.nome = nome;
+    }
+}
